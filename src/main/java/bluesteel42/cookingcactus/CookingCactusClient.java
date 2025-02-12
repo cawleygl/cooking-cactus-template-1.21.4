f78c70a9d54b6201e991c7a503738a8955f7ab2c
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-02-11 23:07:18 -0500

    Initial commit

---
+++ src/main/java/bluesteel42/cookingcactus/CookingCactusClient.java
@@ -0,0 +1,10 @@
+package bluesteel42.cookingcactus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CookingCactusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
